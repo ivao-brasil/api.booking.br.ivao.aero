27d017345b7545f546a881cf4a3acf1bf6b0fdfb
Joelson Carvalho <25236114+joelson-c@users.noreply.github.com>
2022-02-28 11:46:21 -0300

    Fix CORS policy

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -18,29 +18,29 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        // return $next($request);
-
-        $headers = [
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE, PATCH',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age' => '86400',
-            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'
-        ];
-
-        if ($request->isMethod('OPTIONS')) {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
-        }
-
-        foreach ($headers as $key => $value) {
-            if ($response instanceof \Symfony\Component\HttpFoundation\StreamedResponse) {
-                $response->headers->set($key, $value);
-                continue;
-            }
-
-            $response->header($key, $value);
-        }
-
         return $response;
+
+        // $headers = [
+        //     'Access-Control-Allow-Origin' => '*',
+        //     'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE, PATCH',
+        //     'Access-Control-Allow-Credentials' => 'true',
+        //     'Access-Control-Max-Age' => '86400',
+        //     'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'
+        // ];
+
+        // if ($request->isMethod('OPTIONS')) {
+        //     return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        // }
+
+        // foreach ($headers as $key => $value) {
+        //     if ($response instanceof \Symfony\Component\HttpFoundation\StreamedResponse) {
+        //         $response->headers->set($key, $value);
+        //         continue;
+        //     }
+
+        //     $response->header($key, $value);
+        // }
+
+        // return $response;
     }
 }
