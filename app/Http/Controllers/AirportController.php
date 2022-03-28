3eeff181be047946c61e13eb1d0dba590e170f1e
Kewyn Ferreira <kewyn.c@hotmail.com>
2022-03-28 03:14:40 -0300

    Appends the Airport Controller with Distance Calculations

--- app/Http/Controllers/AirportController.php
+++ app/Http/Controllers/AirportController.php
@@ -25,4 +25,52 @@ class AirportController extends Controller
             return $this->hqApi->getAirportDataByIcao($icao);
         });
     }
+
+    //This gets the great circle distance between two airports, in nautical miles
+    public static function getFlightDistance(string $origin, string $destination)
+    {
+        $origin = AirportController::getDetails($origin);
+        $destination = AirportController::getDetails($destination);
+
+        $latDistance = AirportController::getLatDistance($origin['latitude'], $destination['latitude']);
+        $lonDistance = AirportController::getLonDistance($origin['longitude'], $destination['longitude']);
+
+        //Applies pythagorean theorem to find out the distance in degrees.
+        $distance = sqrt( ($latDistance*$latDistance) + ($lonDistance*$lonDistance) );
+
+        //Converts degrees into miles
+        $distance = $distance * 60;
+
+        //Returns the rounded value
+        return round($distance);
+    }
+
+    public static function getLatDistance(float $origin, float $destination){
+        //If both values are positive or negative (on the same hemisphere)
+        if(($origin > 0 && $destination > 0) || ($origin < 0 && $destination < 0))
+        {
+            return abs(abs($origin) - abs($destination));
+        } else {
+            //If they are in different hemisphere
+            return abs(abs($origin) + abs($destination));
+        }
+    }
+
+    public static function getLonDistance(float $origin, float $destination){
+        //If both values are positive or negative (on the same hemisphere)
+        if(($origin > 0 && $destination > 0) || ($origin < 0 && $destination < 0))
+        {
+            $lonDistance =  abs(abs($origin) - abs($destination));
+        } else {
+            //If they are in different hemisphere
+            $lonDistance =  abs(abs($origin) + abs($destination));
+        }
+
+        //If the value is greater than 180 degrees, you must use the other side of the earth
+        if($lonDistance > 180){
+            $lonDistance = abs($lonDistance - 360);
+        }
+
+        return $lonDistance;
+    }
 }
