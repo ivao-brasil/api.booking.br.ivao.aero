e4448a458f23ea4d44758733abd6ad319be32468
Kewyn Ferreira <kewyn.c@hotmail.com>
2022-03-27 23:00:17 -0300

    Creates the aircraft table

---
+++ database/migrations/2022_03_28_015544_create_aircrafts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAircraftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aircraft', function (Blueprint $table) {
+            $table->id();
+            $table->string('iata');
+            $table->string('icao');
+            $table->string('name');
+            $table->integer('speed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aircrafts');
+    }
+}
