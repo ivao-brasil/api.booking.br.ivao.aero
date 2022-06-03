064df42ca67ba755ade870ac6b77ca0aa0ef6906
Kewyn Ferreira <47131740+kewynf@users.noreply.github.com>
2022-06-03 17:21:42 -0300

    Either this fixes everything or I give up

--- app/Models/Slot.php
+++ app/Models/Slot.php
@@ -38,8 +38,6 @@ class Slot extends Model
         'destination'
     ];
 
-    protected $appends = ['flight_time', 'distance', 'timestamps'];
-
     public function owner()
     {
         return $this->belongsTo(User::class, 'pilotId', 'id');
