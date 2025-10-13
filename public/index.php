b18621a3e2ff782ac8bea1c89b756bed2a7013b1
Bruno Ricci <brunoriccidossantos@hotmail.com>
2025-10-12 23:21:53 -0300

    refactor: add public folder

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
@@ -46,7 +45,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
+$kernel = $app->make(\App\Http\Kernel::class);
 
 $response = $kernel->handle(
     $request = Request::capture()
