25368aaf4ec53d65e002d2a1a7fcdb54eb81f682
Bruno Ricci <brunoriccidossantos@hotmail.com>
2025-10-13 00:31:24 -0300

    refactor: add public folder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,18 +41,8 @@ $app->withEloquent();
 */
 
 $app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
-);
-
-$app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
 );
 
 /*
