ebb5d0a0bc5d755a7e38f2096ebc6efbc6c121ad
Bruno Ricci <brunoriccidossantos@hotmail.com>
2025-10-16 19:58:13 -0300

    fix: add logs configuration

---
+++ config/logging.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'default' => env('LOG_CHANNEL', 'single'),
+
+    'channels' => [
+        'single' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+    ],
+];
