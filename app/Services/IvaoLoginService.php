2acf974b41e15ba1cfdba93c50e3e3c6806d7d53
Bruno Ricci <brunoriccidossantos@hotmail.com>
2025-10-16 19:05:58 -0300

    refactor: turn redirect uri to a client param

--- app/Services/IvaoLoginService.php
+++ app/Services/IvaoLoginService.php
@@ -9,12 +9,12 @@ class IvaoLoginService
 {
     private const ENDPOINT = 'https://api.ivao.aero/v2/oauth/token';
 
-    public static function getAccessTokenFromAuthCode($authCode, $codeVerifier = '', $nonce = '')
+    public static function getAccessTokenFromAuthCode($authCode, $redirectUri, $codeVerifier = '', $nonce = '')
     {
         $data = [
             'grant_type' => 'authorization_code',
             'code' => $authCode,
-            'redirect_uri' => env('IVAO_REDIRECT_URI'),
+            'redirect_uri' => $redirectUri,
             'client_id' => env('IVAO_CLIENT_ID'),
             'client_secret' => env('IVAO_CLIENT_SECRET'),
             'scope' => 'profile',
