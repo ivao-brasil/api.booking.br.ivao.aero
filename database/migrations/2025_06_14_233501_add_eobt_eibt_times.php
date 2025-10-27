bd504fa1210a330109af22b013653bcf188586b4
Bruno Ricci <brunoriccidossantos@hotmail.com>
2025-10-26 23:25:29 -0300

    refactor: change slot time structure

--- database/migrations/2025_06_14_233501_add_eobt_eibt_times.php
+++ database/migrations/2025_06_14_233501_add_eobt_eibt_times.php
@@ -15,10 +15,8 @@ class AddeobtEtaTimes extends Migration
     {
         Schema::table('slots', function (Blueprint $table) {
             $table->dropColumn('slottime');
-            $table->dateTime('eobtOrigin')->nullable(true)->after('isFixedDestination');
-            $table->boolean('isFixedeobtOrigin')->default(false)->after('eobtOrigin');
-            $table->dateTime('etaDestination')->nullable(true)->after('isFixedeobtOrigin');
-            $table->boolean('isFixedEtaDestination')->default(false)->after('etaDestination');
+            $table->dateTime('slotTime')->nullable(true)->after('isFixedDestination');
+            $table->boolean('isFixedSlotTime')->default(false)->after('slotTime');
         });
     }
 
@@ -31,10 +29,8 @@ class AddeobtEtaTimes extends Migration
     {
         Schema::table('slots', function (Blueprint $table) {
             $table->string('slottime', 4);
-            $table->dropColumn('eobtOrigin');
-            $table->dropColumn('isFixedeobtOrigin');
-            $table->dropColumn('etaDestination');
-            $table->dropColumn('isFixedEtaDestination');
+            $table->dropColumn('slotTime');
+            $table->dropColumn('isFixedSlotTime');
         });
     }
 }
